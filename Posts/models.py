7468336403feef0db82140561e8d328e9f51b40d
JavierCaramellaBoubet <jacar2000@hotmail.com>
2023-05-03 22:09:51 -0300

    Blog con APP Account_Register_FUNCIONANDO

--- Posts/models.py
+++ Posts/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     subtitulo = models.CharField(max_length=200, null=True, blank= True)
     description = RichTextUploadingField(null=True, blank= True)
-    imagen_portada = models.ImageField(null=True, blank=True, default = "default-image.png")
+    imagen_portada = models.ImageField(null=True, blank=True, default = 'default-image.png')
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False) #Para asignar un ID aleatorio
     fecha_creacion = models.DateTimeField(auto_now_add=True)
 
@@ -19,6 +19,6 @@ class Post(models.Model):
     def __str__(self):
         #texto = "{0} ({1})({2})"
         texto = "{0} ({1})({2})({3})({4})({5})"
-        #return texto.format(self.title,self.subtitulo,self.fecha_creacion)
-        return texto.format(self.title,self.subtitulo,self.description,self.imagen_portada,self.id,self.fecha_creacion)
+        return texto.format(self.title,self.subtitulo,self.fecha_creacion)
+        #return texto.format(self.title,self.subtitulo,self.description,self.imagen_portada,self.id,self.fecha_creacion)
     
--- ProyctoFinalBlog/settings.py
+++ ProyctoFinalBlog/settings.py
@@ -148,9 +148,7 @@ CKEDITOR_UPLOAD_PATH = 'uploads/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-LOGOUT_REDIRECT_URL='Account/AppCoder/'
 
-LOGIN_URL='Account/AppCoder/login/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
